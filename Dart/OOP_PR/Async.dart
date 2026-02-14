8be95c06f7147ff9ba3a622e4e55829e6468e1b0
MuradAhmedTahmim <muradahmedtahmim@gmail.com>
2026-02-14 14:19:46 +0600

    async new code added

--- Dart/OOP_PR/Async.dart
+++ Dart/OOP_PR/Async.dart
@@ -1,11 +1,24 @@
 main() async {
   print('Started');
-  await task1();
+  var result = await Future.wait([task1(), task2(), task3()]);
+  print(result);
   print('End');
 }
 
-task1() {
+Future<void> task1() {
   String value = 'Task-1';
 
-  return Future.delayed(Duration(seconds: 3), () => print(value));
+  return Future.delayed(Duration(seconds: 2), () => (value));
+}
+
+Future<void> task2() {
+  String value = 'Task-2';
+
+  return Future.delayed(Duration(seconds: 3), () => (value));
+}
+
+Future<void> task3() {
+  String value = 'Task-3';
+
+  return Future.delayed(Duration(seconds: 5), () => (value));
 }
